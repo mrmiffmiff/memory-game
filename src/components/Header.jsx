bfb86cfa3a137a0af657090555dbbee9b1d69967
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-12-29 22:12:08 -0800

    Header Structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header({ score, highScore }) {
+    return (
+        <header>
+            <h1>Memory Game</h1>
+            <div className="scores">
+                <span className="score">Score: {score}</span>
+                <span className="high-score">High Score: {highScore}</span>
+            </div>
+        </header>
+    )
+}
