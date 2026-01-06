d8dadca970d374c2ea5d070ad07cf9a63852ecb4
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-01-05 16:29:42 -0800

    header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import '../styles/Header.css'
 export default function Header({ score, highScore }) {
     return (
         <header>
-            <h1>Memory Game</h1>
+            <h1>Code Geass Memory Game</h1>
             <div className="scores">
                 <span className="score">Score: {score}</span>
                 <span className="high-score">High Score: {highScore}</span>
